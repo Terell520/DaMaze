848852d26d2d79e23bf7f6f5adfd8749adf0e28e
Terell520 <terellbrown200@gmail.com>
2023-02-26 13:04:50 -0500

    Update1

--- Assets/dacontrols.cs
+++ Assets/dacontrols.cs
@@ -1,34 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class dacontrols : MonoBehaviour
 {
+    int health = 10;
+    public TMP_Text healthText;
+
+    public float speed = 5f;
+
     float thisISADecimal = 3.14f;
     // Start is called before the first frame update
     void Start()
     {
-     Debug.Log(thisISADecimal);
-     thisISADecimal++;
+        healthText.text = health.ToString();
+    
+    
+     
+      
      
     }
 
     // Update is called once per frame
     void Update()
     {
-     Vector3 newPos = transform.position;
+     Vector3 currentPos = transform.position;
         
             if (Input.GetKey(KeyCode.W))
             {
-            newPos.z+= 0.5f;
+            //newPos.z+= 0.5f;
+            currentPos.z = currentPos.z + speed * Time.deltaTime;
 
         }
-            if (Input.GetKey(KeyCode.S))
-        { newPos.z -= 0.5f; }
-            if (Input.GetKey(KeyCode.A))
-        { newPos.x -= 0.5f; }
-            if (Input.GetKey(KeyCode.D))
-        { newPos.x += 0.5f; }
+        if (Input.GetKey(KeyCode.S))
+            //{ newPos.z - = 0.5f; }
+            currentPos.z = currentPos.z - speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            //{ newPos.x - = 0.5f; }
+            currentPos.x = currentPos.x - speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            //{ newPos.x + = 0.5f; }
+            currentPos.x = currentPos.x + speed * Time.deltaTime;
             
          //newPos.x+= 0.5f;
         
@@ -36,7 +49,16 @@ public class dacontrols : MonoBehaviour
 
      //newPos.y++;
 
-     transform.position = newPos;
+     transform.position = currentPos;
 
+    }
+    void OnCollisionEnter(Collision otherThing)
+    {
+        Debug.Log(otherThing.gameObject.name);
+        if(otherThing.gameObject.name == "Enemy")
+        {
+            health--;
+
+        }
     }
 }
